708fe399d7676552e11c569775245007d0a714cc
minerva88 <46870332+minerva88@users.noreply.github.com>
2022-07-21 01:24:44 +0200

    API & Tailwind config

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Layout from "../components/layout/Layout";
 import Head from "../components/layout/Head";
+import axios from "axios";
+import { BASE_URL } from "../constants/api";
 
-export default function Home() {
+export default function Home(props) {
  return (
   <>
    <Layout>
@@ -9,8 +11,32 @@ export default function Home() {
 
    <div className="container">
     <h1>Home page</h1>
+    {props.albums.map((album) => {
+      return <h3 key={album.id}>{album.attributes.title}</h3>;
+    })}
    </div>
   </Layout>
   </>
  );
+}
+
+export async function getStaticProps() {
+
+  let albums = [];
+
+  try {
+    const response = await axios.get(BASE_URL);
+
+    albums = response.data;
+  } catch(error) {
+    console.log(error);
+  }
+
+  return {
+    props: {
+      albums: albums,
+    },
+  };
+
+
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './pages/**/*.{html,js,jsx,}',
+    './components/**/*{}',
+  ],
   theme: {
+    colors: {
+      'lightgrey': '#E5EAF0',
+      'black': '#000',
+      'darkgrey': '#707070',
+      'offwhite': '#F8F9FA',
+    },
     extend: {},
   },
   plugins: [],
